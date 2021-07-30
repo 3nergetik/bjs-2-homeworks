e4fa1e2c8a42b73e0a7f8a801d94c2a087d11dcd
3nergetik <carbo_r86@mail.ru>
2021-07-30 22:20:01 +0500

    task4 is ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,14 +2,31 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  
+  /* Переменная result принимает true только если:
+  1. Массивы имеют одинаковую длину
+  2. Каждый элемент первого массива равен элементу другого массива на той же позиции (index) */
+
+  result = (arr1.length == arr2.length) && arr1.every((item,index) => {
+  	return item === arr2[index];
+  });
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
   // Ваш код
+
+  resultArr = arr.filter((item) => {
+  	if ((item > 0) && !(item % 3)) {
+  		return item;
+  	}
+  }).map((item) => {
+  	return item * 10;
+  });
 
   return resultArr; // array
 }
+
+advancedFilter(arr);
