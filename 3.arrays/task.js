a762c4925fafb56daa759d5e654718c89a655ea6
3nergetik <carbo_r86@mail.ru>
2021-07-31 16:44:23 +0500

    task 2 and 3 is correct now

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,32 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-  
-  /* Переменная result принимает true только если:
-  1. Массивы имеют одинаковую длину
-  2. Каждый элемент первого массива равен элементу другого массива на той же позиции (index) */
-
-  result = (arr1.length == arr2.length) && arr1.every((item,index) => {
-  	return item === arr2[index];
-  });
-
-  return result; // boolean
+  return (arr1.length == arr2.length) && arr1.every((item,index) => item === arr2[index]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-  // Ваш код
-
-  resultArr = arr.filter((item) => {
-  	if ((item > 0) && !(item % 3)) {
-  		return item;
-  	}
-  }).map((item) => {
-  	return item * 10;
-  });
-
-  return resultArr; // array
+  return arr.filter((item) => item > 0 && !(item % 3)).map((item) => item * 10);
 }
-
-advancedFilter(arr);
