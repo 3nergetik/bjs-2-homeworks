5bdb292a552126aa555c6f1324105fde18be6174
3nergetik <carbo_r86@mail.ru>
2021-08-02 20:37:32 +0500

    task4.1 is ready

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -17,6 +17,15 @@ Student.prototype.addMark = function (mark) {
   }
 }
 
+Student.prototype.addMarks = function (mark) {
+	if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    	this.marks.push(mark);
+  }
+}
+
 Student.prototype.getAverage = function () {
 let sum = 0;
   for (let i = 0; i < this.marks.length; i++) {
