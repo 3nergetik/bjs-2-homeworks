364e0b40ef74de56683bcd18206158457d6153be
3nergetik <carbo_r86@mail.ru>
2021-08-21 19:02:51 +0500

    task 2 is ready

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,49 @@
+/* task1 */
+
+let parseCount = (value) => {
+    if (isNaN(Number.parseInt(value))) {
+        throw new Error("Невалидное значение");
+    }
+        return Number.parseInt(value);
+}
+
+let validateCount = (value) => {
+    try {
+        if (parseCount(value)) {
+            return parseCount(value);
+        }
+    }
+    catch (err) {
+        return err;
+    }
+}
+
+/* task2 */
+class Triangle {
+    constructor(a,b,c) {
+        this.a = a
+        this.b = b
+        this.c = c
+        if (((a + b) < c) || ((a + c) < b) || ((b + c) < a)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    getPerimeter() {
+        return this.a + this.b + this.c
+    }
+    getArea() {
+        let p = (this.a + this.b + this.c) / 2
+        return Number((Math.sqrt(
+            p * (p - this.a) * (p - this.b) * (p - this.c)
+        )).toFixed(3));
+    }
+}
+
+function getTriangle(a,b,c) {
+    try {
+        return new Triangle(a,b,c)
+    }
+    catch (err) {
+        return "Ошибка! Треугольник не существует"
+    }
+}
